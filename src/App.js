73458becf6208de36d7acb192607a3a4535a8a1f
Jorge Arteaga Aranibar <jorgeaar3al@gmail.com>
2022-09-30 01:08:10 -0500

    just need to finish post section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import "./components/Navbar";
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
 import { GetSection } from "./components/GetSection";
+import { PostSection } from "./components/PostSection";
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Hero />
-      <GetSection />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <Hero />
+            <GetSection />
+            <PostSection />
+        </div>
+    );
 }
 
 export default App;
--- src/components/GetSection.js
+++ src/components/GetSection.js
@@ -3,61 +3,64 @@ import { User } from "./User";
 import Defaultphoto from "../assets/defaultphoto.svg";
 
 const GetSection = () => {
-  let [page, setPage] = useState(1);
-  const [users, setUsers] = useState([]);
-  const [totalPages, setTotalPages] = useState(1);
-  const placeholderUrl =
-    "https://frontend-test-assignment-api.abz.agency/images/placeholders/placeholder.png";
-  const getUsers = () => {
-    if (page <= totalPages) {
-      try {
+    let [page, setPage] = useState(1);
+    const [users, setUsers] = useState([]);
+    const [totalPages, setTotalPages] = useState(2);
+    const placeholderUrl =
+        "https://frontend-test-assignment-api.abz.agency/images/placeholders/placeholder.png";
+    const getUsers = () => {
         fetch(
-          `https://frontend-test-assignment-api.abz.agency/api/v1/users?page=${page}&count=6`
+            `https://frontend-test-assignment-api.abz.agency/api/v1/users?page=${page}&count=6`
         )
-          .then((res) => {
-            return res.json();
-          })
-          .then((data) => {
-            setUsers(users.concat(data.users));
-            setTotalPages(data.total_pages);
-          });
-      } catch (e) {
-        console.log(e);
-      }
-    }
-    return;
-  };
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                setUsers(users.concat(data.users));
+                setTotalPages(data.total_pages);
+            });
 
-  useEffect(() => {
-    getUsers();
-  }, [page]);
+        return;
+    };
+    let lastpage = page === totalPages;
+    useEffect(() => {
+        if (page <= totalPages) {
+            getUsers();
 
-  return (
-    <section className="get-section">
-      <h2 className="title">Working with GET request</h2>
-      <article className="people-container">
-        {users.map((user) => (
-          <User
-            key={user.id}
-            name={user.name}
-            email={user.email}
-            img={user.photo == placeholderUrl ? Defaultphoto : user.photo}
-            number={user.phone}
-            position={user.position}
-          />
-        ))}
-      </article>
-      <button
-        children
-        onClick={() => {
-          setPage((page += 1));
-        }}
-        className="btn btn--large"
-      >
-        Show More
-      </button>
-    </section>
-  );
+            console.log(lastpage);
+        }
+    }, [page]);
+
+    return (
+        <section className="get-section">
+            <h2 className="title">Working with GET request</h2>
+            <article className="people-container">
+                {users.map((user) => (
+                    <User
+                        key={user.id}
+                        name={user.name}
+                        email={user.email}
+                        img={
+                            user.photo === placeholderUrl
+                                ? Defaultphoto
+                                : user.photo
+                        }
+                        number={user.phone}
+                        position={user.position}
+                    />
+                ))}
+            </article>
+            <button
+                children
+                onClick={() => {
+                    setPage((page += 1));
+                }}
+                className={!lastpage ? "btn btn--large" : "inactive"}
+            >
+                Show More
+            </button>
+        </section>
+    );
 };
 
 export { GetSection };
